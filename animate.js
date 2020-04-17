7fb0b64f86597063e1c966b24f4d6098fbac2fc2
Timo <timo.schessl@gmx.de>
2020-04-17 09:57:24 +0200

    New Version

--- animate.js
+++ animate.js
@@ -2,7 +2,7 @@ gsap.from("#maintext",{opacity: 0, duration: 2, y: 100})
 gsap.from("#contactform",{opacity: 0, duration: 2, x: 100, delay:0.5})
 gsap.from("#clock",{opacity: 0, duration: 2, x:- 100})
 
-gsap.from("#welcome",{ duration: 3, y: 150, repeat: -1,delay:1})
+gsap.from("#welcome",{ duration: 3, y: -150, repeat: -1,delay:1})
 
 
 
