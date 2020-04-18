c8373e638e7255ad4db2fbc3467f1d6484375882
Timo <timo.schessl@gmx.de>
2020-04-18 11:30:20 +0200

    New animation timing

--- animate.js
+++ animate.js
@@ -1,8 +1,8 @@
-gsap.from("#maintext",{opacity: 0, duration: 2, y: 100})
-gsap.from("#contactform",{opacity: 0, duration: 2, x: 100, delay:0.5})
-gsap.from("#clock",{opacity: 0, duration: 2, x:- 100})
+gsap.from("#maintext",{opacity: 0, duration: 3, y: 20})
+gsap.from("#contactform",{opacity: 0, duration: 1, x: 100, delay:1.5})
+gsap.from("#clock",{opacity: 0, duration: 1, x:- 100, delay: 1})
 
-gsap.from("#welcome",{ duration: 2, y: -150, repeat: -1,delay:1})
+gsap.from("#welcome",{ duration: 1.5, y: -150, repeat: -2,delay:1})
 
 
 
